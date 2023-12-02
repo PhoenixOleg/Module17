3839f6720e811d3ca357063b4d79ca26c477c8f9
PhoenixOleg <metall_2000@mail.ru>
2023-12-02 22:49:14 +0300

    В метод Main добавлен код, чтобы просто что-то делалось. Не холостая работа

--- Module17/Program.cs
+++ Module17/Program.cs
@@ -4,8 +4,26 @@
     {
         static void Main()
         {
+            Account account = new();
+            account.Balance = 999;
+            account.Type = "Обычный";
 
+            Calculator.CalculateInterest(account);
+            Console.WriteLine($@"Ставка счета ""{account.Type}"" при балансе равном {account.Balance} составляет {account.Interest}");
 
+            account.Balance = 1000;
+            account.Type = "Обычный";
+
+            Calculator.CalculateInterest(account);
+            Console.WriteLine($@"Ставка счета ""{account.Type}"" при балансе равном {account.Balance} составляет {account.Interest}");
+
+            account.Balance = 1000;
+            account.Type = "Зарплатный";
+
+            Calculator.CalculateInterest(account);
+            Console.WriteLine($@"Ставка счета ""{account.Type}"" при балансе равном {account.Balance} составляет {account.Interest}");
+
+            Console.ReadKey();
         }
     }
 }
