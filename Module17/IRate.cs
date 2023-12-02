312959a39a0332bb77b49da11d3f683d0ab117c1
PhoenixOleg <metall_2000@mail.ru>
2023-12-02 23:01:08 +0300

    Добавлен интерфейс IRate с методом CalculateInterest для расчета ставки. Входной параметр метода- величина баланса

---
+++ Module17/IRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module17
+{
+    /// <summary>
+    /// Интерфейс расчета процентной ставки
+    /// Расчет идет исходя из величиный баланса на счете
+    /// </summary>
+    public interface IRate
+    {
+        public void CalculateInterest(double balance);
+    }
+}
