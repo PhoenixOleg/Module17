45052f4799c21879cccb58b5da2c1ae9c1cadb52
PhoenixOleg <metall_2000@mail.ru>
2023-12-02 23:23:30 +0300

    Создан класс SalaryAccount реализующий интерфейс IRate

---
+++ Module17/SalaryAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module17
+{
+    public class SalaryAccount : IRate
+    {
+        private double _interest;
+
+        // тип учетной записи
+        public string Type { get; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest
+        {
+            get { return _interest; }
+        }
+
+        public SalaryAccount(double balance)
+        {
+            Type = "Зарплатный";
+            Balance = balance;
+        }
+
+        public void CalculateInterest()
+        {
+            // расчет процентной ставки зарплатного аккаунта по правилам банка
+            _interest = Balance * 0.5;
+        }
+    }
+}
