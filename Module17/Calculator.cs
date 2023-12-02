ac6d615bd251ebd6365dfffcc6c6b38c4309fd75
PhoenixOleg <metall_2000@mail.ru>
2023-12-02 23:28:21 +0300

    Удален класс Account за ненадобностью Удален код из метода CalculateInterest класса Calculator

--- Module17/Calculator.cs
+++ Module17/Calculator.cs
@@ -12,22 +12,7 @@ namespace Module17
         // Метод для расчета процентной ставки
         public static void CalculateInterest(Account account)
         {
-            if (account.Type == "Обычный")
-            {
-                // расчет процентной ставки обычного аккаунта по правилам банка
-                account.Interest = account.Balance * 0.4;
 
-                if (account.Balance < 1000)
-                    account.Interest -= account.Balance * 0.2;
-
-                if (account.Balance >= 1000)
-                    account.Interest -= account.Balance * 0.4;
-            }
-            else if (account.Type == "Зарплатный")
-            {
-                // расчет процентной ставки зарплатного аккаунта по правилам банка
-                account.Interest = account.Balance * 0.5;
-            }
         }
     }
 }
